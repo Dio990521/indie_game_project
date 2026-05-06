a39926edfbe1640572a96558b848e44f87297d28
DCZ <dcz990521jp@gmail.com>
2026-05-06 09:57:02 +0900

    Add Excitement Tile

---
+++ game test/Assets/Scripts/Game/Board/Data/ExcitementTile.cs
@@ -0,0 +1,20 @@
+using IndieGame.Core;
+using IndieGame.Gameplay.ActionPoint;
+using UnityEngine;
+
+namespace IndieGame.Gameplay.Board.Data
+{
+    /// <summary>
+    /// 兴奋格：玩家停下后随机恢复 1~5 点行动点
+    /// </summary>
+    [CreateAssetMenu(menuName = "BoardGame/Tiles/Excitement Tile")]
+    public class ExcitementTile : TileBase
+    {
+        public override void OnPlayerStop(GameObject player)
+        {
+            int amount = Random.Range(1, 6);
+            Debug.Log($"<color=magenta>[Excitement Tile]</color> 玩家 {player.name} 兴奋了！随机恢复 {amount} 点行动点！");
+            ActionPointSystem.Instance.RestoreActionPoints(amount, "ExcitementTile");
+        }
+    }
+}
